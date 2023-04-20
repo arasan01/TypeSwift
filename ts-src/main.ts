5d9177474a644c667adc1e5147871024d2bcdf56
arasan01 <developer@arasan01.dev>
2023-04-21 01:10:53 +0900

    wip

--- ts-src/main.ts
+++ ts-src/main.ts
@@ -1,6 +1,55 @@
 import * as ts from "typescript";
-const program = ts.createProgram(["ts-src/test.ts"], {});
-const source = program.getSourceFile("ts-src/test.ts");
-if (source) {
-  console.log(source.statements);
+
+function getJSON(fileName: string) {
+  const program = ts.createProgram([fileName], {});
+  const checker = program.getTypeChecker();
+
+  const sourceFile = program.getSourceFile(fileName);
+
+  const results: { name: string; type: string }[] = [];
+
+  function visit(node: ts.Node) {
+    if (ts.isFunctionDeclaration(node)) {
+      const symbol = checker.getSymbolAtLocation(node.name!);
+      const type = checker.getTypeOfSymbolAtLocation(symbol!, node.name!);
+      results.push({
+        name: node.name!.getText(),
+        type: checker.typeToString(type),
+      });
+    }
+
+    if (ts.isInterfaceDeclaration(node) || ts.isClassDeclaration(node)) {
+      const symbol = checker.getSymbolAtLocation(node.name!);
+      const type = checker.getTypeOfSymbolAtLocation(symbol!, node.name!);
+      results.push({
+        name: node.name!.getText(),
+        type: checker.typeToString(type),
+      });
+    }
+
+    if (ts.isTypeAliasDeclaration(node)) {
+      const symbol = checker.getSymbolAtLocation(node.name);
+      const type = checker.getTypeOfSymbolAtLocation(symbol!, node.name);
+      results.push({
+        name: node.name.getText(),
+        type: checker.typeToString(type),
+      });
+    }
+
+    ts.forEachChild(node, visit);
+  }
+
+  visit(sourceFile!);
+
+  return JSON.stringify(results, null, 4);
 }
+
+// read command line arguments
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.error("Usage: ts-node main.ts <file.ts>");
+  process.exit(1);
+}
+const filePath = args[0];
+const json = getJSON(filePath);
+console.log(json);
